2caa6ab2e511b301352b4db424ddb5b39118c5a8
JustusMan5 <jho.honkanen@gmail.com>
2025-11-04 17:25:05 +0200

    Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Pressable style={styles.button} onPress={() => setModalVisible(true)}>
+        <Text style={styles.buttonText}>Show modal message</Text>
+      </Pressable>
+
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => { setModalVisible(false); }}
+      >
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContent}>
+            <Text>This is modal!</Text>
+            <Pressable style={[styles.button, styles.closeButton]} onPress={() => setModalVisible(false)}>
+              <Text style={styles.buttonText}>Close</Text>
+            </Pressable>
+
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -16,5 +37,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#fff',
+    paddingVertical: 12,
+    paddingHorizontal: 18,
+    borderRadius: 8,
+    marginTop: 12,
+  },
+  closeButton: {
+    backgroundColor: '#fff',
+    marginTop: 16,
+  },
+  buttonText: {
+    color: '#000000ff',
+    fontWeight: '600',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+    width: '90%',
+    height: '20%',
+    alignItems: 'center',
   },
 });
